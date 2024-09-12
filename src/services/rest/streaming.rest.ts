bfa4784f79530b71be176f20cda895c8a84f158d
Gabriel Pinheiro <mxbiel2000@gmail.com>
2024-09-12 12:36:30 -0300

    feature: api consumption configuration

---
+++ src/services/rest/streaming.rest.ts
@@ -0,0 +1,25 @@
+import type { QueryParams } from '@/models/query-params';
+import api from '@/services/api-config/rxjs-config';
+import type { Observable } from 'rxjs';
+
+export class StreamingRest {
+    getAll(params: QueryParams): Observable<any> {
+        const url: string = `/trending/all/day?page=${ params.page || 1 }&language=pt-BR`;
+        return api.get(url);
+    }
+
+    getMovies(params: QueryParams): Observable<any> {
+        const url: string = `/trending/movie/day?page=${ params.page || 1 }&language=pt-BR`;
+        return api.get(url);
+    }
+
+    getSeries(params: QueryParams): Observable<any> {
+        const url: string = `/trending/tv/day?page=${ params.page || 1 }&language=pt-BR`;
+        return api.get(url);
+    }
+
+    getDetails(params: QueryParams): Observable<any> {
+        const url: string = `/${ params.mediaType }/${ params.id }?append_to_response=videos`;
+        return api.get(url);
+    }
+}
