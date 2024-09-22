80e74a2ad4b4aaaedaec39c327f48e535ca88940
Gabriel Pinheiro <mxbiel2000@gmail.com>
2024-09-22 16:36:27 -0300

    commit develop

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import './assets/main.css'
+import 'index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from "primevue/config";
 
 const app = createApp(App)
 
-app.use(router)
+app.use(PrimeVue, {
+    unstyled: true
+})
 
+app.use(router)
 app.mount('#app')
