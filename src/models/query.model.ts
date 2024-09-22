556544bb4ecbba3d037f738e7bc4c7504ab5c7e9
Gabriel Pinheiro <mxbiel2000@gmail.com>
2024-09-22 17:25:24 -0300

    feature: streaming service configuration

---
+++ src/models/query.model.ts
@@ -0,0 +1,7 @@
+export class QueryParams {
+    constructor(
+        public page?: number,
+        public id?: number,
+        public mediaType?: string
+    ) {}
+}
---
+++ src/views/streaming.service.ts
@@ -0,0 +1,55 @@
+import type { QueryParams } from "@/models/query.model";
+import { StreamingRest } from "@/services/rest/streaming.rest";
+import { BehaviorSubject, Observable, take } from "rxjs";
+
+export class StreamingService {
+  constructor(
+    private _streamings = new StreamingRest() 
+  ) {}
+
+  private streaming$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+
+  streamings: Observable<any> = this.streaming$.asObservable();
+  
+  getAllStreamings(query: QueryParams): void {
+    this._streamings.getAll(query)
+      .pipe(take(1))
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            this.streaming$.next(response);
+          }
+        },
+        error: (err) => {
+          console.error("Falha na requisição", err);
+          alert("Falha na requisição");
+        }
+      });
+  }
+
+    getMovies(query: QueryParams): void {
+        this._streamings.getMovies(query)
+            .pipe(take(1))
+            .subscribe({
+                next: (response) => {
+                    this.streaming$.next(response);
+                },
+                error: () => {
+                    alert("Falha na requisição");
+                }
+            });
+    }
+
+    getSeries(query: QueryParams): void {
+        this._streamings.getSeries(query)
+            .pipe(take(1))
+            .subscribe({
+                next: (response) => {
+                    this.streaming$.next(response);
+                },
+                error: () => {
+                    alert("Falha na requisição");
+                }
+            });
+    }
+}
